5928423ccb359a17989537936b73ce82cf757d5d
quangvlog <quangtoxic98@gmail.com>
2025-04-07 17:46:54 +0700

    Update tool.sh

--- Almalinux/tool.sh
+++ Almalinux/tool.sh
@@ -1,6 +1,9 @@
-#! bin/bash
+#! /bin/bash
 fallocate -l 4G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 echo '/swapfile none swap sw 0 0' >> /etc/fstab
+echo "Da tao file swap thanh cong"
+sudo dnf install -y wget gcc make tar
+echo "Da tcai cac goi can thiet..."
