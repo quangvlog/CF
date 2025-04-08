385d26245611e6b31037f85995a6b7b14b144580
quangvlog <quangtoxic98@gmail.com>
2025-04-08 14:15:18 +0700

    Update ipv6.sh

--- Ubuntu/ipv6.sh
+++ Ubuntu/ipv6.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
+-r /etc/network/interfaces
 read -p "Nhap gia tri ipv6: " ipv6
 read -p "Nhap gia tri netmask: " netmask
 read -p "Nhap gia tri gateway: " gateway
-echo "iface eth0 inet6 static
+touch /etc/network/interfaces
+echo "auto lo
+iface lo inet loopback
+auto eth0
+iface eth0 inet dhcp
+iface eth0 inet6 static
 address $ipv6
 netmask $netmask
 gateway $gateway" >> /etc/network/interfaces
