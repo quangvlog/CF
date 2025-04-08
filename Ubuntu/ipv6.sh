dc46110694f85bdfa28f3e18d09885ae84f4ba26
quangvlog <quangtoxic98@gmail.com>
2025-04-08 12:22:14 +0700

    Update ipv6.sh

--- Ubuntu/ipv6.sh
+++ Ubuntu/ipv6.sh
@@ -4,5 +4,5 @@ read -p "Nhap gia tri cho bien ipv6: " ipv6
 read -p "Nhap gia tri cho bien netmask: " netmask
 read -p "Nhap gia tri cho bien gateway: " gateway
 touch a
-echo -e "ipv6 $ipv6\nnetmask $netmask\ngateway $gateway" >> interfaces
+echo -e "iface eth0 inet6 static\nipv6 $ipv6\nnetmask $netmask\ngateway $gateway" >> interfaces
 service networking restart
